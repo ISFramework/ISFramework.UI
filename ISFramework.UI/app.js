61e0ab13716589c7e13228c5cc3a4fb82ff1e0d8
mohammad reza rahimi <mr_rahimi@live.com>
2017-04-06 23:22:54 +0430

    change layout

--- ISFramework.UI/app.js
+++ ISFramework.UI/app.js
@@ -1,1 +1,15 @@
 ﻿var app = angular.module('app', ['esPanel','esTextbox']);
+app.run(function ($rootScope) {
+    $rootScope.direction = 'ltr';
+});
+app.controller('layout', ['$rootScope', function ($rootScope) {
+    $rootScope.toggleDirection = function () {
+        if ($rootScope.direction == 'rtl'){
+            $rootScope.direction = 'ltr';
+        }
+        else {
+            $rootScope.direction = 'rtl';
+
+        }
+    }
+}]);
