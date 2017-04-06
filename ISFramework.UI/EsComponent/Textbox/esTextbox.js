0622fc2f7568669d219d25a1d4ce3a0d5cc12c7d
mohammad reza rahimi <mr_rahimi@live.com>
2017-04-06 22:41:12 +0430

    Add project files.

---
+++ ISFramework.UI/EsComponent/Textbox/esTextbox.js
@@ -0,0 +1,36 @@
+﻿(function () {
+var module = angular.module('esTextbox', []);
+module.directive('esTextbox', ["$http", "$parse", "$compile", "$interpolate",
+    function ($http, $parse, $compile, $interpolate) {
+        return {
+            restrict: 'A',
+            controller: function ($scope, $element, $attrs) {
+            },
+            compile: function (tElem, tAttrs) {
+                return {
+                    pre: function (scope, iElem, iAttrs) {
+
+                    },
+                    post: function (scope, iElem, iAttrs, Ctrl) {
+                        if (iAttrs.textboxIcon) {
+                            var placeholderIcon = $interpolate(iAttrs.textboxIcon)(scope);
+                            iAttrs.$observe('textboxIcon', function (value) {
+                                placeholderIcon = value;
+                            });
+                        }
+                        iElem.addClass('form-control');
+                        var texInputWrapper = angular.element("<div class='es-text-input-wrapper'>");
+                        iElem.wrap(texInputWrapper);
+
+                        if (placeholderIcon) {
+                            $(iElem).parent(".es-text-input-wrapper").addClass("es-icon-placeholder-left");
+                            var texInputIcon = angular.element("<span class='es-icon-placeholder'></span>");
+                            texInputIcon.addClass(placeholderIcon);
+                            iElem.after(texInputIcon);
+                        }
+                    }
+                }
+            },
+        }
+    }]);
+})();
---
+++ ISFramework.UI/EsComponent/panel/esPanel.js
@@ -0,0 +1,71 @@
+﻿var esPanel = angular.module('esPanel', []);
+esPanel.directive('esPanel', ["$interpolate",function ($interpolate) {
+    return {
+        restrict: "E",
+        replace: true,
+        template:'<div></div>',
+        transclude: true,
+        link: function (scope, element, attr, ctrl, transclude) {
+            element.addClass("panel panel-default");
+            transclude(function (clone) {
+                element.append(clone);
+            });
+        }
+    }
+}]);
+esPanel.directive('panelHeading', [function () {
+    return {
+        restrict: "E",
+        replace: true,
+        template: '<div></div>',
+        transclude: true,
+        link: function (scope, element, attr, ctrl, transclude) {
+            element.addClass("panel-heading");
+            transclude(function (clone) {
+                element.append(clone);
+            });
+        }
+    }
+}]);
+esPanel.directive('panelBody', [function () {
+    return {
+        restrict: "E",
+        replace: true,
+        template: '<div></div>',
+        transclude: true,
+        link: function (scope, element, attr, ctrl, transclude) {
+            element.addClass("panel-body");
+            transclude(function (clone) {
+                element.append(clone);
+            });
+        }
+    }
+}]);
+esPanel.directive('panelTitle', [function () {
+    return {
+        restrict: "E",
+        replace: true,
+        template: '<div></div>',
+        transclude: true,
+        link: function (scope, element, attr, ctrl, transclude) {
+            element.addClass("panel-title");
+            transclude(function (clone) {
+                element.append(clone);
+            });
+        }
+    }
+}]);
+esPanel.directive('panelFooter', [function () {
+    return {
+        restrict: "E",
+        replace: true,
+        template: '<div></div>',
+        transclude: true,
+        link: function (scope, element, attr, ctrl, transclude) {
+            element.addClass("panel-footer");
+            transclude(function (clone) {
+                element.append(clone);
+            });
+        }
+    }
+}]);
